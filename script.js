8480377b350b1e6aca1adfc24f88f5b756e58b80
YaroslavSigidin <sigidingo@gmail.com>
2026-04-13 16:55:57 +0800

    Убрать Тропический джин-тоник

--- script.js
+++ script.js
@@ -885,7 +885,7 @@ const menuImageItems = [
   },
   // Коктейли в слайдере
   { src: 'assets/images/2/ermitazh.jpg', aliases: ['Эрмитаж', 'Коктейль Эрмитаж'] },
-  { src: 'assets/images/2/romovaya-baba.jpg', aliases: ['Тропический джин-тоник', 'Тропический джин‑тоник', 'Ромовая Баба', 'Ромовая баба'] },
+  { src: 'assets/images/2/romovaya-baba.jpg', aliases: ['Ромовая Баба', 'Ромовая баба'] },
   { src: 'assets/images/2/rozovaya-pantera.jpg', aliases: ['Розовая Пантера', 'Розовая пантера'] },
   { src: 'assets/images/2/slezy-byvshego.jpg', aliases: ['Слезы бывшего', 'Слёзы бывшего'] },
   { src: 'assets/images/2/severnoe-siyanie.jpg', aliases: ['Северное сияние'] },
